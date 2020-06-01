6167c35a24464a4a206f8eed2ca443b61d15cac3
kiku ishimoto <kikuish@gmail.com>
2020-06-01 16:04:13 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr. Chocolate', 45, 'female', 'pink', 'angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Rowdy', 20, 'male', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Tabitha', 20, 'female', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Sergeant Brown', 50, 'male', 'brown', 'calm', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Melissa', 20, 'female', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Grinch', 20, 'male', 'brown', 'mad', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Wendy', 20, 'male', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES(null, 20, 'male', 'brown', 'calm', 1)
